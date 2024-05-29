0dd3ce1f28f5e663b74fcf59e2d2c905cc769ec6
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2024-05-29 11:43:52 +0200

    chore: Add health endpoint to API

--- app/api.py
+++ app/api.py
@@ -15,6 +15,10 @@ def hello(request: Request):
         "url": str(request.url)
     }
 
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=4444)
